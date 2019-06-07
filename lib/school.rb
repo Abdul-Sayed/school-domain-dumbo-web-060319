bcc5cd7dbbf87d821b151819bf885f6a43860a45
Abdul <awsayed@alaska.edu>
2019-06-07 00:50:02 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -5,10 +5,4 @@ class School
     @roster = {}
   end
 
-  def add_student(student_name, grade)
-    @student_name = student_name
-    @grade = grade
-    roster[@grade] = []
-    roster[@grade].push(student_name)
-  end
 end
